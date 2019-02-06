71864b56ef2325140555f3f0a59606a95cc62a11
fmatusiak <f.matusiak95@gmail.com>
2019-02-06 23:53:40 +0100

    correction TaskController

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     private Long id;
     private String title;
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
