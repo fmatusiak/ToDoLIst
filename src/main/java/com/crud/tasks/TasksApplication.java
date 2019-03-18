1191e953ae2eb426732a91e49e2a3f383ad51e08
fmatusiak <f.matusiak95@gmail.com>
2019-03-18 13:53:26 +0100

    add new version task application to heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 @SpringBootApplication
 public class TasksApplication{
 
